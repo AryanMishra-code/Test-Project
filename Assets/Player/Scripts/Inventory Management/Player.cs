50a2dc325a406c602b8c586915a9b6c9448261b9
Aryan Mishra <aryanm0806@gmail.com>
2022-05-01 16:55:08 +0530

    Inventory System Part 1

---
+++ Assets/Player/Scripts/Inventory Management/Player.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public InventoryObject inventory;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<Item>();
+
+        if (item)
+        {
+            inventory.AddItem(item.item, 1);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
+    }
+}
