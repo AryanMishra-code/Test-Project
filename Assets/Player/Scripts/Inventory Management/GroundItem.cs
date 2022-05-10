60a2a89ca51cb7417b9b2c04bc89b3de77598d9b
Aryan Mishra <aryanm0806@gmail.com>
2022-05-10 15:29:41 +0530

    Some Changes In Prefabs

--- Assets/Player/Scripts/Inventory Management/GroundItem.cs
+++ Assets/Player/Scripts/Inventory Management/GroundItem.cs
@@ -1,22 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
+public class GroundItem : MonoBehaviour
 {
     public ItemObject item;
-    public void OnBeforeSerialize()
-    {
-        // GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
-        // EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
-    }
-
-    public void OnAfterDeserialize()
-    {
-    }
-
+    
     public void Start()
     {
         var objectPrefab = Instantiate(item.objectPrefab, this.transform.position, Quaternion.identity);
