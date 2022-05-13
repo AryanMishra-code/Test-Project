2cebfb569c5c71f6c10e7af98983b13a3e51aba7
Aryan Mishra <aryanm0806@gmail.com>
2022-05-13 18:51:22 +0530

    Making The Hotbar before I mess up again in equipment

--- Assets/Player/Scripts/Inventory Management/GroundItem.cs
+++ Assets/Player/Scripts/Inventory Management/GroundItem.cs
@@ -7,7 +7,7 @@ public class GroundItem : MonoBehaviour
 {
     public ItemObject item;
     
-    public void Start()
+    void Start()
     {
         var objectPrefab = Instantiate(item.objectPrefab, this.transform.position, Quaternion.identity);
         objectPrefab.transform.SetParent(this.transform);
